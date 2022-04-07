087efb52b75d184e0a7ac94d4cdad19be8829a40
MargueriteLaVache <mayeul.hospital@free.fr>
2022-04-07 22:18:39 +0200

    Comments in CellClasses package.

--- src/CellClasses/CellObstacle.java
+++ src/CellClasses/CellObstacle.java
@@ -1,10 +1,21 @@
 package CellClasses;
 
+
 import java.awt.*;
 import java.awt.image.BufferedImage;
 
+
+/**
+ * Class CellObstacle
+ * Extends class CellContent
+ * Represents the presence of an obstacle on a cell
+ */
 public class CellObstacle extends CellContent {
-    /** constructor */
+    /**
+     * Class constructor of the CellObstacle class
+     * @param c : cell in which this object is
+     * @param i : image to be displayed at the location of the cell
+     */
     public CellObstacle (Cell c, BufferedImage i)  {
         super(c,i);
     }
--- src/CellClasses/CellRessource.java
+++ src/CellClasses/CellRessource.java
@@ -1,11 +1,19 @@
 package CellClasses;
 
-import Threads.IncreaseRessource;
 
+import Threads.IncreaseRessource;
 import java.awt.image.BufferedImage;
 import java.util.Timer;
 
+
+/**
+ * Class CellResource
+ * Extends class CellContent
+ * Represents the presence of a resource source on the cell
+ * Contains information about the resource  in the cell
+ */
 public class CellRessource extends CellContent {
+    /* Declaration of variables representing the state of the resource present on the cell */
     RessourceType ressourceType;
     final int maxAmount;
     int currentAmount;
@@ -13,7 +21,14 @@ public class CellRessource extends CellContent {
     Timer timer;
     IncreaseRessource increaseRessource;
 
-    /** constructor */
+
+    /**
+     * Class constructor of the class CellResource
+     * @param r : type of the resource on the cell
+     * @param c : cell corresponding to this object
+     * @param i : image representing the resource, to be displayed on the cell in the view
+     * @param max : maximum quantity of the resource that can be present in the cell
+     */
     public CellRessource (RessourceType r, Cell c, BufferedImage i, int max)  {
         super(c,i);
         this.ressourceType = r;
@@ -25,25 +40,35 @@ public class CellRessource extends CellContent {
         timer.schedule(this.increaseRessource, 0, 5000);
     }
 
-    /** Method getCurrentAmount
-     * returns the current amount of the ressource
-     * @return currentAmount;
+
+    /**
+     * Function getCurrentAmount
+     * Returns the current amount of the resource
+     * @return currentAmount : the current amount of the resource on the cell
      */
     public int getCurrentAmount(){ return currentAmount; }
 
-    /** Method takeRessource
-     *
+
+    /**
+     * Method takeResource
+     * Diminishes the amount of the resource in line with the value passed in parameters
+     * @param value : quantity of resource to be taken
      */
     public void takeRessource(int value){ currentAmount -= value; }
 
-    /** Method takeRessource
-     *
+
+    /**
+     * Method addResource
+     * Increases the amount of the resource in line with the value passed in parameters
+     * @param value : quantity of resource to be added
      */
     public void addRessource(int value){ currentAmount += value; }
 
-    /** Method getRessourceType
-     * returns the ressource type
-     * @return ressourceType;
+
+    /**
+     * Function getResourceType
+     * Returns the type of the resource
+     * @return resourceType : the type of the resource in the cell
      */
     public RessourceType getRessourceType(){ return ressourceType; }
 
